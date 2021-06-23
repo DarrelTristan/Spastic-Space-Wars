95a557efd882ec357152524fcd20c782aed1fc81
Windiarta <75001432+Windiarta@users.noreply.github.com>
2021-06-23 20:34:29 +0700

    Add files via upload

---
+++ howtoplay.h
@@ -0,0 +1,79 @@
+//Airell Ramadhan Budiraharjo - 2006535230
+
+void how_to_play (){
+		printf(".___________. __    __  .___________.  ______    .______       __      ___       __  \n");
+		printf("|           ||  |  |  | |           | /  __  \\   |   _  \\     |  |    /   \\     |  | \n");
+		printf("`---|  |----`|  |  |  | `---|  |----`|  |  |  |  |  |_)  |    |  |   /  ^  \\    |  | \n");
+		printf("    |  |     |  |  |  |     |  |     |  |  |  |  |      /     |  |  /  /_\\  \\   |  | \n");
+		printf("    |  |     |  `--'  |     |  |     |  `--'  |  |  |\\  \\----.|  | /  _____  \\  |  `----.\n");
+		printf("    |__|      \\______/      |__|      \\______/   | _| `._____||__|/__/     \\__\\ |_______|\n");
+	
+		printf("\n%s%s","Spastic Space Wars merupakan video game 2D yang bertemakan luar angkasa. Player dapat mengerakkan spaceship\n",
+		"secara horizontal untuk menghancurkan asteroid yang sedang bergerak menuju spaceship.\n");
+	
+		printf ("\n------------ HOW TO PLAY --------------\n>>Berikut adalah langkah-langkah untuk bermain mode Single Player :\n");
+		printf ("1. Pilih 'Single Player' yang tersedia di menu\n");
+		printf ("2. Setelah itu anda sudah dapat memulai permainan");
+		printf ("\n\n>>Berikut adalah langkah-langkah untuk bermain mode Local Multiplayer :\n");
+		printf ("1. Pilih 'Local MultiPlayer' yang tersedia di menu\n");
+		printf ("2. Setelah itu anda sudah dapat memulai permainan dalam mode 2 pemain");
+	
+		printf ("\n\n\n\n------------ RULES OF PLAY ------------\n>>Berikut adalah aturan permainan Single Player :\n\n");
+		printf("\t|                                        |\n");
+		printf("\t|                                        |\n");
+		printf("\t|                                        |\n");
+		printf("\t|                                        |\n");
+		printf("\t|                                        |\n");
+		printf("\t|                                        |\n");
+		printf("\t|                                        |\n");
+		printf("\t|                                        |\n");
+		printf("\t|                  'w'                   |\n");
+		printf("\t|                   ^                    |\n");
+		printf("\t|                                        |\n");
+		printf("\t|                   ^                    |\n");
+		printf("\t|                                        |\n");
+		printf("\t|                   ^                    |\n");
+		printf("\t|               'a'   'd'                |\n");
+		printf("\t|               <-- A -->                |\n");
+		puts("");
+		printf ("1. Player dapat mengerakkan spaceship dengan menekan tombol 'A' untuk ke arah kiri dan \n");
+		printf ("   tombol 'D' untuk ke arah kanan.\n");
+		printf ("2. Untuk menembak player dapat menekan tombol 'W'\n");
+		printf ("3. Arahkan spaceship ke pesawat musuh agar dapat dihancurkan\n");
+		printf ("4. Setiap asteroid yang dihancurkan akan mendapatkan poin 50\n");
+		printf ("5. Pemain dapat mengumpulkan poin sebanyak-banyaknya\n");
+		printf ("6. Permainan akan berakhir ketika laser musuh mengenai spaceship player\n");
+		
+		printf ("\n\n\n>>Berikut adalah aturan permainan Local Multiplayer :\n\n");
+		printf("\t|                                       ||                                       |\n");
+		printf("\t|                                       ||                                       |\n");
+		printf("\t|                                       ||                                       |\n");
+		printf("\t|                                       ||                                       |\n");
+		printf("\t|                                       ||                                       |\n");
+		printf("\t|                                       ||                                       |\n");
+		printf("\t|                                       ||                                       |\n");	
+		printf("\t|                                       ||                                       |\n");
+		printf("\t|                 'w'                   ||                 'i'                   |\n");
+		printf("\t|                  ^                    ||                  ^                    |\n");
+		printf("\t|                                       ||                                       |\n");
+		printf("\t|                  ^                    ||                  ^                    |\n");
+		printf("\t|                                       ||                                       |\n");
+		printf("\t|                  ^                    ||                  ^                    |\n");
+		printf("\t|              'a'   'd'                ||              'j'   'l'                |\n");
+		printf("\t|              <-- A -->                ||              <-- A -->                |\n");
+		puts("");
+		printf ("1. Player 1 dapat mengerakkan spaceship dengan menekan tombol 'A' untuk ke arah kiri dan \n");
+		printf ("   tombol 'D' untuk ke arah kanan.\n");
+		printf ("2. Player 2 dapat mengerakkan spaceship dengan menekan tombol 'J' untuk ke arah kiri dan \n");
+		printf ("   tombol 'L' untuk ke arah kanan.\n");
+		printf ("3. Untuk menembak player 1 dapat menekan tombol 'W'\n");
+		printf ("4. Untuk menembak player 2 dapat menekan tombol 'I'\n");
+		printf ("5. Arahkan spaceship ke asteroid agar dapat dihancurkan\n");
+		printf ("6. Setiap asteroid yang dihancurkan akan mendapatkan poin 50\n");
+		printf ("7. Pemain dapat mengumpulkan poin sebanyak-banyaknya\n");
+		printf ("8. Permainan akan berakhir ketika laser musuh mengenai spaceship player\n");
+		
+		printf("\n\n\nPress any key to continue...");
+		getch();
+		system("cls");
+}
