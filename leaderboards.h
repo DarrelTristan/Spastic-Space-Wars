5b1ea57b7b202579d465a7de9d2406b7b0ce5005
Airell Ramadhan B <71022178+airellrmdn@users.noreply.github.com>
2021-06-23 21:19:38 +0700

    Add files via upload

--- leaderboards.h
+++ leaderboards.h
@@ -62,14 +62,14 @@ void sortList() {
             	    strcpy(current->s,index->s);
             	    strcpy(index->s,temp2);
                 }
-		else if(current->data == index->data && strcmpi(current->s,index->s) > 0) {  
+				else if(current->data == index->data && strcmpi(current->s,index->s) > 0) {  
                     temp = current->data;  
                     current->data = index->data;  
                     index->data = temp;
-					
-		    temp1 = current->time;
+				
+		    		temp1 = current->time;
                	    current->time = index->time;
-		    index->time = temp1;  
+		    		index->time = temp1;  
                 
                     strcpy(temp2,current->s);
             	    strcpy(current->s,index->s);
@@ -91,12 +91,12 @@ void openf(){
 	fptr = fopen("ScoreText.txt","r");
 	if(fptr == NULL){                  //create file if the file doesn't exist
 		fclose(fptr);
-	 	fopen("ScoreText.txt","w");
-	 	fclose(fptr);
+		fopen("ScoreText.txt","w");
+		fclose(fptr);
 	}
 	else {
 		int c = getc(fptr);
-		while (c != EOF) {	//Looping when not reaching end of file 
+		while (c != EOF) {	    //Looping when not reaching end of file 
    			fscanf(fptr, "%s %d %f\n", nama, &score, &waktu);  //read file
 			addNode(nama, score, waktu);
 			c = getc(fptr);	
@@ -113,7 +113,7 @@ void write (FILE *fptr){
         return;  
     }
     while(current != NULL) {    //write the data from linked list to the text file
-	fprintf(fptr, "- %10s\t %d\t %.2f\n", current->s, current->data, current->time);
+		fprintf(fptr, "- %10s\t %d\t %.2f\n", current->s, current->data, current->time);
         current = current->next;  
     } 
 	fclose(fptr);   
@@ -124,13 +124,13 @@ void display (){
 	int i = 1;
 	printf ("\n\n\n\t%3s\t%10s\t%s\t%s\n", "NO.", "NAME", "SCORE", "TIME");
 	printf ("\t======================================\n");
-	while(current != NULL) {		//print data from linked list
-	      printf("\t%3d\t", i);
-	      printf("%10s\t", current->s);
-    	      printf("%d\t", current->data);
-	      printf("%.2f\n", current->time);
-	      current = current->next;	
-              i++;
+	while(current != NULL) {				//print data from linked list
+	    printf("\t%3d\t", i);
+	    printf("%10s\t", current->s);
+        printf("%d\t", current->data);
+        printf("%.2f\n", current->time);
+	    current = current->next;	
+        i++;
 	}  
 	head = NULL;
 	free(current);
@@ -141,16 +141,14 @@ void display (){
       
 void leaderboard (char name[10], float *time, int *globalScore){
     FILE *fptr;  
-	
     struct node *current = head;
     head = NULL;
-    openf();                                     //Check text file and read text file to get data for linked list
+    openf();                                    //Check text file and read text file to get data for linked list
 
-    addNode(name, *globalScore, *time);          //add linked list
+    addNode(name, *globalScore, *time);         //add linked list
 
-      
-    sortList();    		                     //Sorting list (descending)
-    write(fptr);    			             //Write leaderboard to the text
+    sortList();    		             //Sorting list (descending)
+    write(fptr);    			     //Write leaderboard to the text
     free(head);					     //Free the data on linked list
     getch();
     system("cls");
